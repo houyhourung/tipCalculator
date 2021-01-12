c06fbfcc0111925fbbb733a11dc0ce0a66d1c090
houyhourung <houyhourung88@gmail.com>
2021-01-12 17:04:47 -0500

    Complete UI and add feature where the user can split among 100 people max

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -18,6 +18,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var billAmountTextField: UITextField!
     
+    @IBAction func peopleStepper(_ sender: UIStepper) {
+        //display number of people
+       numPeopleTextField.text = String(NSNumber(value: sender.value).stringValue)
+    }
+   
+    @IBOutlet weak var numPeopleTextField: UILabel!
+    
     @IBOutlet weak var tipPercentageLabel: UILabel!
     
     @IBOutlet weak var totalLabel: UILabel!
@@ -29,10 +36,10 @@ class ViewController: UIViewController {
         //get intial billAmount
         let bill = Double(billAmountTextField.text!) ?? 0
         let tipPercentage = [0.15,0.18,0.2]
-        
+        //calculating tips
         let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
-       let total = bill + tip
-        
+        let people = Double(numPeopleTextField.text!) ?? 1
+        let total = (bill + tip) / people
         tipPercentageLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
